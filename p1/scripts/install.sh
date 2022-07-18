183cb25e51116d8ccf0b5c819b013de3a5bd7165
Pierre TRICHET <pitriche@e2r9p3.clusters.42paris.fr>
2022-07-18 17:40:49 +0200

    scripts now run for each users (whoami instead of hardcoded users)

--- p1/scripts/install.sh
+++ p1/scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-qemu-img create -f qcow2 /sgoinfre/goinfre/Perso/frthierr/iot.qcow2 30G
+qemu-img create -f qcow2 /sgoinfre/goinfre/Perso/$(whoami)/iot.qcow2 30G
 
 qemu-system-x86_64 -smp cores=2 \
 				   -m 4G \
 				   -nic user \
 				   -boot d \
-				   -cdrom /sgoinfre/goinfre/Perso/frthierr/archlinux-2022.07.01-x86_64.iso \
-				   -hda /sgoinfre/goinfre/Perso/frthierr/iot.qcow2 \
+				   -cdrom /sgoinfre/goinfre/Perso/$(whoami)/archlinux-2022.07.01-x86_64.iso \
+				   -hda /sgoinfre/goinfre/Perso/$(whoami)/iot.qcow2 \
 				   -display gtk \
 				   -enable-kvm
--- p1/scripts/run.sh
+++ p1/scripts/run.sh
@@ -3,7 +3,7 @@
 qemu-system-x86_64 -smp cores=2 \
 				   -m 4G \
 				   -nic user \
-				   -hda /sgoinfre/goinfre/Perso/frthierr/iot.qcow2 \
+				   -hda /sgoinfre/goinfre/Perso/$(whoami)/iot.qcow2 \
 				   -display gtk \
 				   -accel hvf \
 				   -cpu host \
