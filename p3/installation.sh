f60ff46c0b0cd802a70f6ebd7ada08ffd1693d6d
hpottier <hpottier@student.42.fr>
2022-07-22 22:26:10 +0200

    Making the app

--- p3/installation.sh
+++ p3/installation.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-#docker
+echo -e "\nINSTALLING DOCKER"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum makecache
 sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl enable --now docker
 
-#kubectl
+echo -e "\nSTARTING DOCKER"
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+
+echo -e "\nINSTALLING KUBECTL"
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 sudo yum install -y kubectl
 
-#k3d
+echo -e "\nINSTALLING K3D"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-#Let user use docker commands without sudo
-sudo usermod -aG docker $USER
+echo -e "\nINSTALLATION DONE"
--- p3/setup.sh
+++ p3/setup.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-echo "CREATING K3D CLUSTER"
+echo -e "\nCREATING K3D CLUSTER"
 k3d cluster create p3-cluster --k3s-arg "--disable=traefik@server:0"
 
-# echo "WAITING FOR JOBS TO COMPLETE"
-# kubectl wait --for=condition=complete jobs --all -n kube-system --timeout=-1s
-
-echo "CREATING NAMESPACES"
+echo -e "\nCREATING NAMESPACES"
 kubectl create namespace argocd
 kubectl create namespace dev
 
-echo "INSTALLING ARGO CD NO TLS"
-# kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
+echo -e "\nINSTALLING ARGO CD WITH NO TLS"
 kubectl apply -f argoinstall.yaml -n argocd
 
-echo "WAITING FOR ARGO CD DEPLOYMENTS TO BE AVAILABLE"
+echo -e "\nWAITING FOR ARGO CD DEPLOYMENTS TO BE AVAILABLE"
 kubectl wait --for=condition=available deployment --all -n argocd --timeout=-1s
 
-echo "WAITING FOR ARGO CD PODS TO BE READY"
+echo -e "\nWAITING FOR ARGO CD PODS TO BE READY"
 kubectl wait --for=condition=ready pods --all -n argocd --timeout=-1s
 
-echo "CONNECTING TO ARGO CD SERVER"
-kubectl port-forward svc/argocd-server -n argocd 8080:80 &
-# kubectl apply -f argoingress.yaml -n argocd
-# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+echo -e "\nARGO CD SERVER PORT FORWARDING"
+kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:80 2>&1 > /dev/null &
 
-# echo "WAITING FOR ARGO CD DEPLOYMENTS TO BE AVAILABLE"
-# kubectl wait --for=condition=available deployment --all -n argocd --timeout=-1s
+echo -e "\nSTARTING APP"
+kubectl apply -f wil42-playground.yaml -n argocd
 
-# echo "WAITING FOR ARGO CD PODS TO BE READY"
-# kubectl wait --for=condition=ready pod --all -n argocd --timeout=-1s
+echo -e "\nARGO CD PASSWORD, CONNECT AS 'admin'"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
